53c4457c193063e667091409c8419d8213d2d1ac
yoel Pingle <ypingle@hotmail.com>
2018-01-08 12:06:12 +0000

    new version

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 
 	for(int i = 0; i<3 ; i++){
-		System.out.println("hello 1.1.2018 no " + i);
+		System.out.println("hello 8.1.2018 14:00 no " + i);
 
 	}
 
